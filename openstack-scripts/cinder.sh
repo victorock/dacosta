5e39878777f439c2ed5b6d6db672112de4633348
victorock <root@vicdacosta>
2015-08-06 17:03:53 +0200

    training

---
+++ openstack-scripts/cinder.sh
@@ -0,0 +1,48 @@
+keystone service­-create --­­name cinder --­­type volume --­­description "OpenStack Block Storage"
+​keystone service­-create --­­name cinderv2 --­­type volumev2 ­­description "OpenStack Block Storage"
+
+
+keystone endpoint-­create --­­service-­id \
+$(keystoneservice­list|awk'/volume/{print$2}') \
+--­­public-url http://Controller:8776/v1/%\(tenant_id\)s \
+--­­internal-url http://Controller:8776/v1/%\(tenant_id\)s \ ­­
+--admin-url http://Controller:8776/v1/%\(tenant_id\)s \
+--­­region regionOne
+
+keystone endpoint-­create --­­service-­id \
+$(keystoneservice­list|awk'/volume/{print$2}') \
+--­­public-url http://Controller:8776/v2/%\(tenant_id\)s \
+--­­internal-url http://Controller:8776/v2/%\(tenant_id\)s \ ­­
+--admin-url http://Controller:8776/v2/%\(tenant_id\)s \
+--­­region regionOne
+
+
+apt-­get install cinder-­api \
+cinder-­scheduler python-­cinderclient ­-y
+
+crudini --set /etc/cinder/cinder.conf database connection mysql://cinder:roipass@Controller/cinder
+crudini --set /etc/cinder/cinder.conf DEFAULT rpc_backend rabbit
+crudini --set /etc/cinder/cinder.conf DEFAULT rabbit_host Controller
+crudini --set /etc/cinder/cinder.conf DEFAULT rabbit_password roipass
+crudini --set /etc/cinder/cinder.conf DEFAULT my_ip 11.0.0.41
+crudini --set /etc/cinder/cinder.conf DEFAULT glance_host Controller
+crudini --set /etc/cinder/cinder.conf DEFAULT verbose True
+crudini --set /etc/cinder/cinder.conf DEFAULT auth_strategy keystone
+
+crudini --set /etc/cinder/cinder.conf keystone_authtoken auth_uri http://Controller:5000/v2.0
+crudini --set /etc/cinder/cinder.conf keystone_authtoken identity_uri http://Controller:35357
+crudini --set /etc/cinder/cinder.conf keystone_authtoken admin_tenant_name service
+crudini --set /etc/cinder/cinder.conf keystone_authtoken admin_user cinder
+crudini --set /etc/cinder/cinder.conf keystone_authtoken admin_password roipass
+
+crudini --delete /etc/cinder/cinder.conf ​auth_host
+crudini --delete /etc/cinder/cinder.conf ​​​auth_port
+crudini --delete /etc/cinder/cinder.conf ​auth_protocol​
+
+
+su ­-s /bin/sh -­c "cinder­manage db sync" cinder
+rm -f /var/lib/cinder/cinder.sqlite
+
+
+service cinder­-scheduler restart
+service cinder­-api restart
